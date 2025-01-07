e52dad9419ca7d405b4b5fcdf9addd0a2c76def8
arthurnov <art.dsky@gmail.com>
2025-01-07 21:39:40 +0200

    added fire rate limiter

--- player.py
+++ player.py
@@ -10,6 +10,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
     
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -25,6 +26,11 @@ class Player(CircleShape):
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
+        if self.timer > 0:
+            self.timer -= dt
+        else:
+            self.timer = 0
+
         if keys[pygame.K_w] or keys[pygame.K_UP]:
             self.move(dt)
         if keys[pygame.K_a] or keys[pygame.K_LEFT]:
@@ -33,7 +39,7 @@ class Player(CircleShape):
             self.move(-dt)    
         if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
             self.rotate(dt)
-        if keys[pygame.K_SPACE]:
+        if keys[pygame.K_SPACE] and self.timer == 0:
             self.shoot()
 
 
@@ -45,5 +51,6 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
     
     def shoot(self):
+        self.timer = PLAYER_SHOOT_COOLDOWN
         bullet = Shot(self.position.x, self.position.y)
         bullet.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
