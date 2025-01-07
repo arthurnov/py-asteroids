44a76917872b1c9bbb47c99628059a944f8a26ee
arthurnov <art.dsky@gmail.com>
2025-01-07 20:12:10 +0200

    added basic ship to asteroid collision

--- main.py
+++ main.py
@@ -34,6 +34,11 @@ def main():
         for obj in updatable:
             obj.update(dt)
 
+        for obj in asteroids:
+            if obj.collision(player):
+                print("Game Over")
+                return
+
         screen.fill("black")
 
         for obj in drawable:
